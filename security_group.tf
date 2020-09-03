3d1eb270124e2e4ed49496ecdcd223ffd5ca9913
Benedetto <bennesp@users.noreply.github.com>
2020-09-03 09:51:30 +0200

    style: formatting also security group tf

--- security_group.tf
+++ security_group.tf
@@ -3,32 +3,32 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_security_group_rule" "ssh_cidr" {
-  type = "ingress"
-  protocol = "tcp"
+  type              = "ingress"
+  protocol          = "tcp"
   security_group_id = aws_security_group.sg.id
-  from_port = 22
-  to_port = 22
-  cidr_blocks = var.in_cidr_blocks
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = var.in_cidr_blocks
 }
 
 resource "aws_security_group_rule" "ssh_source_security_group" {
   count = length(var.in_source_security_group) > 0 ? 1 : 0
 
-  type = "ingress"
-  protocol = "tcp"
-  security_group_id = aws_security_group.sg.id
-  from_port = 22
-  to_port = 22
+  type                     = "ingress"
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg.id
+  from_port                = 22
+  to_port                  = 22
   source_security_group_id = var.in_source_security_group
 }
 
 resource "aws_security_group_rule" "out_all" {
-  type = "egress"
-  protocol = -1
+  type              = "egress"
+  protocol          = -1
   security_group_id = aws_security_group.sg.id
-  from_port = 0
-  to_port = 0
-  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 locals {
@@ -38,15 +38,15 @@ locals {
 }
 
 resource "aws_security_group_rule" "custom_ports" {
-  count = length(local.not_ssh_open_ports)
-  type = "ingress"
-  protocol = "tcp"
+  count             = length(local.not_ssh_open_ports)
+  type              = "ingress"
+  protocol          = "tcp"
   security_group_id = aws_security_group.sg.id
   from_port = "${2 == length(split("-", element(local.not_ssh_open_ports, count.index))) ?
-      element(split("-", element(local.not_ssh_open_ports, count.index)), 0) :
-      element(local.not_ssh_open_ports, count.index) }"
+    element(split("-", element(local.not_ssh_open_ports, count.index)), 0) :
+  element(local.not_ssh_open_ports, count.index)}"
   to_port = "${2 == length(split("-", element(local.not_ssh_open_ports, count.index))) ?
-      element(split("-", element(local.not_ssh_open_ports, count.index)), 1) :
-      element(local.not_ssh_open_ports, count.index) }"
+    element(split("-", element(local.not_ssh_open_ports, count.index)), 1) :
+  element(local.not_ssh_open_ports, count.index)}"
   cidr_blocks = var.in_cidr_blocks
 }
