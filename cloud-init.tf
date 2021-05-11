91eb963feb0d35c613c4e2b5c307619ff02abb2c
Benedetto <bennesp@users.noreply.github.com>
2021-05-11 15:45:16 +0200

    remove useless sudo in cloud init

--- cloud-init.tf
+++ cloud-init.tf
@@ -22,7 +22,7 @@ data "template_cloudinit_config" "config" {
           owner: root:root
           permissions: '0644'
       runcmd:
-        - [sudo, docker, run, -d, --restart, always, -v, /etc/cwagentconfig:/etc/cwagentconfig, amazon/cloudwatch-agent:1.247347.6b250880]
+        - [docker, run, -d, --restart, always, -v, /etc/cwagentconfig:/etc/cwagentconfig, amazon/cloudwatch-agent:1.247347.6b250880]
     EOF
   }
 }
