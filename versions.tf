b04b0bdf291bdfb390a999189b655a79ac38da23
Benedetto <bennesp@users.noreply.github.com>
2021-05-11 15:05:53 +0200

    add aws version, upgrade terraform version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.39.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = "~> 0.15"
 }
